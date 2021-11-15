bdbe8f44fc8e5d398325b2e35504a51ff317fc02
RKlopka <RKlopka@GRE-X-L-KLOPKR>
2021-11-15 11:23:45 -0600

    Add project files.

---
+++ ChuckNorris/GetChuckNorris.cs
@@ -0,0 +1,21 @@
+﻿using System.Management.Automation;
+
+namespace ChuckNorris
+{
+    [Cmdlet(VerbsCommon.Get,"ChuckNorris")]
+    [OutputType(typeof(ChuckNorrisInfo))]
+    public class GetChuckNorris : Cmdlet
+    {
+
+    }
+    public class ChuckNorrisInfo
+    {
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+        public List<String> categories { get; set; }
+        public DateTime? created_at { get; set; }
+        public DateTime? updated_at { get; set; }
+    }
+}
